c0c0b5932af6b5bfe5ad3a024e383bfa635568ec
a0m0e6u <alex.matheson@walmart.com>
2020-10-30 15:00:15 -0500

    ext feature

--- zsh-vsc-dif.zsh
+++ zsh-vsc-dif.zsh
@@ -1,9 +1,63 @@
 PLUGIN_LOCATION=${0:A:h}
+LIST_OF_FILE_EXTS=("
+    c 
+    cc  
+    class 
+    clj 
+    cpp 
+    cs 
+    cxx 
+    diff 
+    el        
+    go 
+    h 
+    htm
+    html 
+    java 
+    js 
+    js 
+    json 
+    l
+    log 
+    m 
+    m4 
+    md 
+    patch 
+    php 
+    pl 
+    po 
+    py 
+    rb 
+    rs 
+    rtf 
+    sh 
+    swift
+    svg 
+    ts 
+    ts 
+    tsx 
+    txt 
+    ua 
+    vb 
+    vcxproj 
+    xcodeproj 
+    xml 
+    yml 
+")   
 
 function dif() {
     if [ $# -eq 0 ]; then
-        exec code --diff $PLUGIN_LOCATION/dif1 $PLUGIN_LOCATION/dif2
+        touch $PLUGIN_LOCATION/dif1.txt  $PLUGIN_LOCATION/dif2.txt 
+        code --diff $PLUGIN_LOCATION/dif1.txt $PLUGIN_LOCATION/dif2.txt 
+        exit
     fi
+ 
+    if [[ " ${LIST_OF_FILE_EXTS[@]} " =~ " $1 " ]]; then
+        touch $PLUGIN_LOCATION/dif1.$1  $PLUGIN_LOCATION/dif2.$1 
+        code --diff $PLUGIN_LOCATION/dif1.$1 $PLUGIN_LOCATION/dif2.$1
+        exit
+    fi
+
     DEFAULT_FILE=`echo $PLUGIN_LOCATION/dif.${1##*.}`
     ARG1=`echo $1`
     ARG2=`echo ${2:-$DEFAULT_FILE}`
