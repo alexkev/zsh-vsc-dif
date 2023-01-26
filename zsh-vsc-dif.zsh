b27534ce17fe916db7cc804b32884e2774dbe491
a0m0e6u <alex.matheson@walmart.com>
2023-01-26 09:55:32 -0600

    creating output folder

--- zsh-vsc-dif.zsh
+++ zsh-vsc-dif.zsh
@@ -1,25 +1,28 @@
 PLUGIN_LOCATION=${0:A:h}
+OUTPUT_LOCATION=$(dirname $PLUGIN_LOCATION)/zsh-vsc-dif-output
 
 function dif() {
-    if [ "$1" = "delete" ]; then 
-        exec mv dif* ~/.Trash 
-    fi 
+    mkdir -p $OUTPUT_LOCATION
+
+    if [ "$1" = "delete" ]; then
+        exec mv $OUTPUT_LOCATION/dif* ~/.Trash
+    fi
 
     if [ $# -eq 0 ]; then
-        touch $PLUGIN_LOCATION/dif1.txt  $PLUGIN_LOCATION/dif2.txt 
-        code --diff $PLUGIN_LOCATION/dif1.txt $PLUGIN_LOCATION/dif2.txt 
-        exit
-    fi
- 
-    if [[ "$1" == .* ]]; then
-        touch $PLUGIN_LOCATION/dif1$1  $PLUGIN_LOCATION/dif2$1 
-        code --diff $PLUGIN_LOCATION/dif1$1 $PLUGIN_LOCATION/dif2$1
+        touch $OUTPUT_LOCATION/dif1.txt $OUTPUT_LOCATION/dif2.txt
+        code --diff $OUTPUT_LOCATION/dif1.txt $OUTPUT_LOCATION/dif2.txt
         exit
     fi
 
-    DEFAULT_FILE=`echo $PLUGIN_LOCATION/dif.${1##*.}`
-    ARG1=`echo $1`
-    ARG2=`echo ${2:-$DEFAULT_FILE}`
+    if [[ "$1" == .* ]]; then
+        touch $OUTPUT_LOCATION/dif1$1 $OUTPUT_LOCATION/dif2$1
+        code --diff $OUTPUT_LOCATION/dif1$1 $OUTPUT_LOCATION/dif2$1
+        exit
+    fi
+
+    DEFAULT_FILE=$(echo $OUTPUT_LOCATION/dif.${1##*.})
+    ARG1=$(echo $1)
+    ARG2=$(echo ${2:-$DEFAULT_FILE})
     if [[ ! -e $DEFAULT_FILE ]]; then
         touch $DEFAULT_FILE
     fi
